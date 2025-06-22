46bbdd8a4d56fa412865352a725e5fee5f60f78c
abdulaziz <aaldhalaan@deem.sa>
2025-06-22 08:36:48 +0300

    chore: remove duplicate action

--- src/main/java/com/abdulaziz1928/builder/actions/DeleteFlagAction.java
+++
@@ -1,9 +0,0 @@
-package com.abdulaziz1928.builder.actions;
-
-import java.util.List;
-
-public class DeleteFlagAction extends AbstractFlagAction {
-    public DeleteFlagAction(List<String> flags) {
-        super(flags);
-    }
-}
